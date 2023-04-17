c989833161b607283f528a96f8b1f0f761d84405
Matt Kline <matt@bitbashing.io>
2023-04-16 22:27:15 -0700

    use ouroboros lib for self-referncing LibState

--- crates/shim/src/lib.rs
+++ crates/shim/src/lib.rs
@@ -2,41 +2,43 @@ use libloading;
 use mlua::prelude::{LuaResult, LuaTable};
 use mlua::Lua;
 use std::path::Path;
+use ouroboros::self_referencing;
 
+#[self_referencing]
 struct LibState {
     lib: libloading::Library,
-    start: libloading::Symbol<'static, extern "C" fn(config::Config) -> u32>,
-    stop: libloading::Symbol<'static, extern "C" fn() -> u32>,
+    #[borrows(lib)]
+    #[covariant]
+    start: libloading::Symbol<'this, extern "C" fn(config::Config) -> u32>,
+    #[borrows(lib)]
+    #[covariant]
+    stop: libloading::Symbol<'this, extern "C" fn() -> u32>,
     // on_frame: libloading::Symbol<'static, unsafe extern "C" fn() -> u32>,
 }
 
 static mut LIB_STATE: Option<LibState> = None;
 
-fn load_export<T>(lib: &libloading::Library, name: &[u8]) -> libloading::Symbol<'static, T> {
-    unsafe { std::mem::transmute(lib.get::<T>(name).unwrap()) }
+fn load_export<'a, T>(lib: &'a libloading::Library, name: &[u8]) -> libloading::Symbol<'a, T> {
+    unsafe { lib.get::<T>(name).unwrap() }
 }
 
 #[no_mangle]
 pub fn start(lua: &Lua, config: config::Config) -> LuaResult<i32> {
     let dll_path = Path::new(config.dll_path.as_str()).join("yawe.dll");
     unsafe {
-        let lib = libloading::Library::new(dll_path).unwrap();
-        let start = load_export(&lib, b"start");
-        let stop = load_export(&lib, b"stop");
-
-        LIB_STATE = Some(LibState {
-            lib: lib,
-            start: start,
-            stop: stop,
-        });
+        LIB_STATE = Some(LibStateBuilder {
+            lib: libloading::Library::new(dll_path).unwrap(),
+            start_builder: |l: &libloading::Library| load_export(l, b"start"),
+            stop_builder: |l: &libloading::Library| load_export(l, b"stop"),
+        }.build());
+        let res = LIB_STATE.as_ref().unwrap().borrow_start()(config);
+        Ok(res as i32)
     }
-
-    Ok(0)
 }
 
 #[no_mangle]
 pub fn stop(lua: &Lua, config: config::Config) -> LuaResult<i32> {
-    Ok(0)
+    Ok(unsafe { LIB_STATE.as_ref().unwrap() }.borrow_stop()() as i32)
 }
 
 #[mlua::lua_module]
