8f2f141f1e63d973b40adae01c18538d9f390019
Matt Kline <matt@bitbashing.io>
2023-04-17 22:43:14 -0700

    Don't turn to salt or something

--- crates/shim/src/lib.rs
+++ crates/shim/src/lib.rs
@@ -1,34 +1,62 @@
-use libloading;
 use mlua::prelude::{LuaResult, LuaTable};
 use mlua::Lua;
+use std::os::windows::ffi::OsStrExt;
 use std::path::Path;
+use windows::Win32::Foundation::{FARPROC, HMODULE};
+use windows::Win32::System::LibraryLoader::{GetProcAddress, LoadLibraryW};
 
 struct LibState {
-    lib: libloading::Library,
-    start: libloading::Symbol<'static, extern "C" fn(config::Config) -> u32>,
-    stop: libloading::Symbol<'static, extern "C" fn() -> u32>,
+    lib: HMODULE,
+    start: Symbol<extern "C" fn(config::Config) -> u32>,
+    stop: Symbol<extern "C" fn() -> u32>,
     // on_frame: libloading::Symbol<'static, unsafe extern "C" fn() -> u32>,
 }
 
 static mut LIB_STATE: Option<LibState> = None;
 
-fn load_export<T>(lib: &libloading::Library, name: &[u8]) -> libloading::Symbol<'static, T> {
-    unsafe { std::mem::transmute(lib.get::<T>(name).unwrap()) }
+fn load_library(p: &Path) -> windows::core::Result<HMODULE> {
+    use windows::core::PCWSTR;
+    let wide_path: Vec<u16> = p.as_os_str().encode_wide().chain(Some(0)).collect();
+    unsafe { LoadLibraryW(PCWSTR(wide_path.as_ptr())) }
+}
+
+struct Symbol<T> {
+    pointer: FARPROC,
+    _pd: std::marker::PhantomData<T>,
+}
+
+impl<T> std::ops::Deref for Symbol<T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        assert!(self.pointer.is_some());
+        // Shield your eyes.
+        unsafe { &*((&self.pointer) as *const FARPROC as *const T) }
+    }
+}
+
+fn load_export<T>(lib: HMODULE, sym: &[u8]) -> Symbol<T> {
+    use windows::core::PCSTR;
+
+    // Compile-time check?
+    assert_eq!(std::mem::size_of::<T>(), std::mem::size_of::<FARPROC>());
+
+    let terminated = std::ffi::CString::new(sym).expect("Null bytes in sym");
+    let pointer = unsafe { GetProcAddress(lib, PCSTR(terminated.as_ptr() as *const u8)) };
+    Symbol {
+        pointer,
+        _pd: std::marker::PhantomData,
+    }
 }
 
 #[no_mangle]
 pub fn start(lua: &Lua, config: config::Config) -> LuaResult<i32> {
     let dll_path = Path::new(config.dll_path.as_str()).join("yawe.dll");
     unsafe {
-        let lib = libloading::Library::new(dll_path).unwrap();
-        let start = load_export(&lib, b"start");
-        let stop = load_export(&lib, b"stop");
+        let lib = load_library(&dll_path).unwrap();
+        let start = load_export(lib, b"start");
+        let stop = load_export(lib, b"stop");
 
-        LIB_STATE = Some(LibState {
-            lib: lib,
-            start: start,
-            stop: stop,
-        });
+        LIB_STATE = Some(LibState { lib, start, stop });
     }
 
     Ok(0)
